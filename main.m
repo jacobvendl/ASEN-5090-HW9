acf510b0b41652e841268f24d15d2ad5d88e1bfc
Jacob <jacob.vendl@colorado.edu>
2019-11-18 20:12:55 -0700

    changed to West 105

--- main.m
+++ main.m
@@ -10,7 +10,7 @@ clear all; close all; clc
 fn = 6.625e6;
 IF = -60e3;
 
-lat=40.0; long = 105.15; alt = 1629;
+lat=40.0; long = -105.15; alt = 1629;
 GPS_LLA = [lat; long; alt]';
 GPS_ECEF = lla2ecef(GPS_LLA);
 
