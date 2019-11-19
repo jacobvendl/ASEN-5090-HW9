ca5f1b8ed808a4b865381bec2836a45281feb82a
Jacob <jacob.vendl@colorado.edu>
2019-11-18 19:33:11 -0700

    test in section 1

--- main.m
+++ main.m
@@ -14,13 +14,13 @@ lat=40.0; long = 105.15; alt = 1629;
 GPS_LLA = [lat; long; alt];
 GPS_ECEF = lla2ecef(GPS_LLA);
 
-%data collected on August 28th, 2018 at 16:29 UTC
-[gps_ephem,gps_ephem_cell] = read_GPSyuma(yumafilename);
 
 
 %% ========================================================================
 % Problem 1 - Visiblity Prediction
 %==========================================================================
+%data collected on August 28th, 2018 at 16:29 UTC
+[gps_ephem,gps_ephem_cell] = read_GPSyuma(yumafilename);
 
 
 %% ========================================================================
